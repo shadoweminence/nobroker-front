998dfdf2f9489c9c284630aef709513842e29768
Prashant <pokhrelprashant9@gmail.com>
2024-10-30 10:17:47 +0545

    trying to solve vercel error

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-bootstrap", "react-icons", "react-icons/fa"],
-    },
-  },
+  build: {},
 });
