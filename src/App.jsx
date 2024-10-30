96416f6f8c63e4622ac8dd6fece2ee0d8b2d954f
Rabi Roy <rrabir9815@gmail.com>
2024-10-30 12:52:11 +0545

    postproperty file added to pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,14 @@ import PayRent from "./pages/PayRent";
 import Navb from "./components/Navbar/Navb";
 import Homepage from "./components/FrontPage/Homepage";
 // import Navbar from "./components/Navbar/Navbar";
-import PostProperty from "./components/postProperty";
+import PostProperty from "./pages/postProperty";
 
 function App() {
   return (
     <>
       <Navb />
-      {/* <Homepage/> */}
-      <PayRent />
-=======
-     <Navb/>
-     {/* <Homepage/> */}
       {/* <PayRent /> */}
       <PostProperty />
-
-      {/* <Navbar /> */}
     </>
   );
 }
