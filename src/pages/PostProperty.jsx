5392726fd1c5b90ec095b2b7a35f48993ebe8b3b
Rabi Roy <rrabir9815@gmail.com>
2024-11-01 18:51:40 +0545

    postProperty page completed

--- src/pages/PostProperty.jsx
+++ src/pages/PostProperty.jsx
@@ -4,6 +4,9 @@ import "react-phone-input-2/lib/style.css";
 import "../style/postProperty.css";
 import { useState } from "react";
 import { Carousel } from "react-bootstrap";
+import QuickChart from '../components/QuickChat'
+import PostPropertyFAQ from "../components/postPropertyFAQ";
+
 
 function PostProperty() {
   const [selectedType, setSelectedType] = useState("Residential");
@@ -13,21 +16,27 @@ function PostProperty() {
     Commercial: ["Rent", "Sale"],
     "Land/Plot": ["Resale"],
   };
-  const brokerFeature = {
-    imgs: [
-      "https://assets.nobroker.in/static/img/listing/icon1.png",
-      "https://assets.nobroker.in/static/img/listing/icon2.png",
-      "https://assets.nobroker.in/static/img/listing/icon3.png",
-    ],
-    title: ["No Calls From Brokers", "Verified Tenants", "Save Brokerage"],
-    content: [
-      "Your information is kept confidential and you no longer have to worry about call from brokers.",
-      "Thanks to our technology, only verified and genuine tenants can call you.",
-      "No Brokers means No Brokerage! Buy yourself a gift",
-    ],
-  };
+
+  const brokerFeature = [
+    {
+      img: "https://assets.nobroker.in/static/img/listing/icon1.png",
+      title: "No Calls From Brokers",
+      content: "Your information is kept confidential and you no longer have to worry about call from brokers.",
+    },
+    {
+      img: "https://assets.nobroker.in/static/img/listing/icon2.png",
+      title: "Verified Tenants",
+      content: "Thanks to our technology, only verified and genuine tenants can call you.",
+    },
+    {
+      img: "https://assets.nobroker.in/static/img/listing/icon3.png",
+      title: "Save Brokerage",
+      content: "No Brokers means No Brokerage! Buy yourself a gift",
+    },
+  ];
   return (
     <>
+    <QuickChart />
       <div className="container" style={{ marginTop: "100px" }}>
         <div className="top d-flex align-items-center justify-content-between">
           <h1 className="fw-semibold fs-4" style={{ color: "#424949" }}>
@@ -357,25 +366,25 @@ function PostProperty() {
         </div>
 
         <div className="row mt-5">
-          {brokerFeature.imgs.map((img, index) => (
+          {brokerFeature.map((item, index) => (
             <div
               key={index}
               className="feature col d-flex flex-column align-items-center text-center"
             >
               <img
                 className="mb-3"
-                style={{ height: "30px", height: "50px" }}
-                src={img}
-                alt={brokerFeature.title[index]}
+                style={{ width: "50px", hight: "70px" }}
+                src={item.img}
+                alt={item.title}
               />
               <h5
                 className="fw-semibold"
                 style={{ fontSize: "18px", color: "#424949" }}
               >
-                {brokerFeature.title[index]}
+                {item.title}
               </h5>
               <p className="" style={{ fontSize: "15px", color: "#787676" }}>
-                {brokerFeature.content[index]}
+                {item.content}
               </p>
             </div>
           ))}
@@ -653,10 +662,70 @@ function PostProperty() {
             </strong>
           </h2>
 
-          <p style={{color: "#767979"}}>NoBroker makes it easy to sell property online or list your property for rent with a simple, streamlined process that connects you directly with tenants or buyers. Here’s how it works:</p>
+          <p style={{ color: "#767979" }}>
+            NoBroker makes it easy to sell property online or list your property
+            for rent with a simple, streamlined process that connects you
+            directly with tenants or buyers. Here’s how it works:
+          </p>
 
-          <h3 className="fs-6" style={{color:"#464949"}}><span><strong>1. Easy Property Listing Process</strong></span></h3>
-          <p style={{color:"#767979"}}>Post a free property ad on NoBroker within minutes. Simply fill out our easy-to-use form with all the relevant details, including location, amenities, and photos. Your property advertisement will go live after a quick verification.</p>
+          <h3 className="fs-6" style={{ color: "#464949" }}>
+            <span>
+              <strong>1. Easy Property Listing Process</strong>
+            </span>
+          </h3>
+          <p style={{ color: "#767979" }}>
+            Post a free property ad on NoBroker within minutes. Simply fill out
+            our easy-to-use form with all the relevant details, including
+            location, amenities, and photos. Your property advertisement will go
+            live after a quick verification.
+          </p>
+
+          <h3 className="fs-6" style={{ color: "#464949" }}>
+            <span>
+              <strong>2. Tenant/Buyer Schedules an Appointment</strong>
+            </span>
+          </h3>
+          <p style={{ color: "#767979" }}>
+            Once your rent or sale listing is live, interested tenants or buyers
+            can easily request your contact details to arrange a visit. Both
+            parties will receive contact information, enabling them to connect
+            directly.
+          </p>
+
+          <p className="py-2" style={{ color: "#767979" }}>Here’s how NoBroker ensures that tenants and buyers find your listing:</p>
+
+          <p className="ms-4 mb-0" style={{ color: "#767979" }}><span style={{color: "#464949"}}><strong>1. Potential Tenant/Buyer Views the Listing:</strong></span> Potential tenants or buyers browse NoBroker’s extensive list of property advertisements, filtering by location, price, and property type to find their ideal match.</p>
+
+          <p className="ms-4 mb-0" style={{ color: "#767979" }}><span style={{color: "#464949"}}><strong>2. They Initiate Direct Contact: </strong></span> Once they find a suitable listing, they can click to view more details and obtain contact information.</p>
+
+          <p className="ms-4" style={{ color: "#767979" }}><span style={{color: "#464949"}}><strong>3. You Receive Alerts:</strong></span> As soon as someone shows interest, you’ll be notified through multiple channels, including SMS, phone, and WhatsApp, so you never miss a lead.</p>
+
+          <h3 className="fs-6 mb-1" style={{color: "#464949"}}><strong>3. Property Visit and Deal Closure</strong></h3> 
+
+          <p className="mb-4" style={{ color: "#767979" }}>After connecting, the property owner and tenant or buyer can arrange a site visit. NoBroker helps facilitate a smooth deal closure by providing additional services like rental agreement creation and doorstep delivery, painting services, cleaning services, etc. By avoiding middlemen, you get to sell your property or rent out property quickly and efficiently, without any brokerage fees.</p>
+
+          <h3 className="fs-5 py-3" style={{color: "#464949"}}><strong>Types of Property Listings Available on NoBroker</strong></h3>
+
+          <p style={{ color: "#767979" }}>Here's an overview of the property listings available on NoBroker:</p>
+
+          <h3 className="fs-6 pb-2" style={{color: "#464949"}}><strong>1. Residential Properties</strong></h3>
+          <p style={{ color: "#767979" }}>If you are looking to rent out property or sell your house, NoBroker provides a seamless solution. You can easily create a listing for various residential types, including:</p>
+
+          <ul style={{listStyleType: "disc"}}>
+            <li style={{color: "#464949"}}>Homes for Rent or Sale: <span style={{color: "#767979"}}>NoBroker’s platform makes it straightforward to post property for rent or sell house online, reaching a broad audience without any brokerage fees.</span></li>
+            <li style={{color: "#464949"}}>PG or Hostel Rentals: <span style={{color: "#767979"}}>Have a paying guest accommodation or hostel space? List it on NoBroker with relevant details. Your rent advertisement will be live in minutes, attracting potential tenants quickly.</span></li>
+          </ul>
+
+        <h3 className="fs-6 pb-2" style={{color: "#464949"}}><strong>2. Commercial Properties</strong></h3>
+          <p style={{ color: "#767979" }}>Sell or rent your commercial property effortlessly on NoBroker’s platform, designed to accommodate a range of business spaces. Whether it's office space, a co-working setup, a retail shop, a showroom, or even a warehouse, you can easily list your commercial property for sale or rent.</p>
+
+          <h3 className="fs-6 pb-2" style={{color: "#464949"}}><strong>3. Plots</strong></h3>
+          <p style={{ color: "#767979" }}>NoBroker makes it easy for you to sell land online with a user-friendly listing system. When you list your plot, you can include important details like its location, size, shape, and other relevant information.</p>
+
+          <p style={{ color: "#767979" }}>If you want to sell property online or find the right tenant quickly, NoBroker offers a seamless, easy-to-use, and cost-effective solution. With zero brokerage, dedicated support, and easy listing features, you can connect directly with potential buyers or tenants. </p>
+
+        <PostPropertyFAQ />
+         
         </div>
       </div>
     </>
