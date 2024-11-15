edea8d0fa70f5b43d35c56025909721b9003cec1
Rabi Roy <rrabir9815@gmail.com>
2024-11-15 18:39:04 +0545

    updated

--- src/components/ReceiptrentCcard.jsx
+++ src/components/ReceiptrentCcard.jsx
@@ -1,119 +1,87 @@
-import React from 'react'
-import '../style/rentReceipt.css'
-import card from '../assets/images/card.png'
+import React from "react";
+import "../style/rentReceipt.css";
+import card from "../assets/images/card.png";
 
 function ReceiptrentCcard() {
+  const cardDetails = [
+    {
+      imgSrc:
+        "https://assets.nobroker.in/nb-new/public/RentoPay/no_hidden_cost.svg",
+      title: "Earn Rewards",
+      desc: "Earn miles, get cash back and reward points on every rent payment with your card.",
+    },
+    {
+      imgSrc:
+        "https://assets.nobroker.in/nb-new/public/RentoPay/credit_card.svg",
+      title: "Pay with Credit Card",
+      desc: "We accept major cards like Visa and Mastercard so your rent payment is hassle-free.",
+      extraImg: card,
+    },
+    {
+      imgSrc:
+        "https://assets.nobroker.in/nb-new/public/RentoPay/digital_rent_receipt.svg",
+      title: "Digital Rent Receipts",
+      desc: "Generate rent receipts instantly and claim your HRA with ease. Get rent receipts sent directly to your email ID.",
+    },
+    {
+      imgSrc:
+        "https://assets.nobroker.in/nb-new/public/RentoPay/no_broker_trust.svg",
+      title: "NoBroker Trust",
+      desc: "NoBroker Pay is a product by NoBroker and is 100% secure with PCI compliant payment gateway.",
+    },
+  ];
+
   return (
-    <>
-      <div className="container mt-5 rent-receipt ">
-      <div className="">
-          <h1>Pay Rent With Credit Card Online</h1>
-          <h4>Earn money just by paying your rent!</h4>
-          <div className="card-section">
-            <div className="row horizontal-slide">
-              <div className="col-lg-6 col-md-6 col-sm-12 d-flex gap-4 mb-5">
+    <div className="container mt-5">
+      <div className="rent-receipt">
+        <h1 className="ms-2">Pay Rent With Credit Card Online</h1>
+        <h4 className="ms-2">Earn money just by paying your rent!</h4>
+        <div className="card-section ">
+          <div className="row horizontal-slide d-flex justify-content-center align-items-center">
+            {cardDetails.map((card, index) => (
+              <div key={index} className="cards d-flex gap-4 text-white">
                 <div className="img">
-                  <div className="img-box d-flex flex-column justify-content-center align-items-center">
+                  <div className="img-box d-flex justify-content-center align-items-center">
                     <img
                       className="img-fluid ms-3"
-                      style={{ height: "50px", width: "42px" }}
-                      src="https://assets.nobroker.in/nb-new/public/RentoPay/no_hidden_cost.svg"
-                      alt="Earn Rewards"
+                      src={card.imgSrc}
+                      alt={card.title}
+                      style={{
+                        maxHeight: "50px",
+                        maxWidth: "42px",
+                        objectFit: "contain",
+                      }}
                     />
                   </div>
                 </div>
-                <div className="">
-                  <div className="title">Earn Rewards</div>
-                  <p className="sub-title">
-                    Earn miles, get cash back and reward points on every rent
-                    payment with your card.
-                  </p>
-                </div>
-              </div>
 
-              <div className="col-lg-6 col-md-6 col-sm-12 d-flex gap-4 mb-5">
-                <div className="img">
-                  <div className="img-box d-flex flex-column justify-content-center align-items-center">
-                    <img
-                      className="img-fluid ms-3"
-                      style={{ height: "50px", width: "42px" }}
-                      src="https://assets.nobroker.in/nb-new/public/RentoPay/credit_card.svg"
-                      alt="Pay with Credit Card"
-                    />
+                <div>
+                  <div className="title d-flex align-items-center">
+                    {card.title}
+                    {card.extraImg && (
+                      <img
+                        src={card.extraImg}
+                        alt="description"
+                        className="ms-4"
+                        style={{ height: "23px", width: "80px" }}
+                      />
+                    )}
                   </div>
-                </div>
-                <div className="">
-                  <div className="d-flex title">
-                    Pay with Credit Card
-                    <img
-                      style={{ height: "23px", width: "80px" }}
-                      className="ms-4"
-                      src={card}
-                      alt="description"
-                    />
-                  </div>
-                  <p className="sub-title">
-                    We accept major cards like Visa and Mastercard so your rent
-                    payment is hassle free.
-                  </p>
+                  <p className="sub-title">{card.desc}</p>
                 </div>
               </div>
-
-              <div className="col-lg-6 col-md-6 col-sm-12 d-flex gap-4 mb-5">
-                <div className="img">
-                  <div className="img-box d-flex flex-column justify-content-center align-items-center">
-                    <img
-                      className="img-fluid ms-3"
-                      style={{ height: "50px", width: "42px" }}
-                      src="https://assets.nobroker.in/nb-new/public/RentoPay/digital_rent_receipt.svg"
-                      alt="Digital Rent Receipts"
-                    />
-                  </div>
-                </div>
-                <div className="">
-                  <div className="title">Digital Rent Receipts</div>
-                  <p className="sub-title">
-                    Generate rent receipts instantly and claim your HRA with
-                    ease. Get rent receipts sent directly to your email ID.
-                  </p>
-                </div>
-              </div>
-
-              <div className="col-lg-6 col-md-6 col-sm-12 d-flex gap-4 mb-5">
-                <div className="img">
-                  <div className="img-box d-flex flex-column justify-content-center align-items-center">
-                    <img
-                      className="img-fluid ms-3"
-                      style={{ height: "50px", width: "42px" }}
-                      src="https://assets.nobroker.in/nb-new/public/RentoPay/no_broker_trust.svg"
-                      alt="NoBroker Trust"
-                    />
-                  </div>
-                </div>
-                <div className="">
-                  <div className="title">NoBroker Trust</div>
-                  <p className="sub-title">
-                    NoBroker Pay is a product by NoBroker and is 100% secure
-                    with PCI compliant payment gateway.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="get-startedbtn d-flex justify-content-center">
-              <button
-                class="getting-started btn btn-primary"
-                style={{ width: "40%" }}
-                id=""
-                type="button"
-              >
-                Get Started
-              </button>
-            </div>
+            ))}
+          </div>
+          <div className="get-startedbtn d-flex justify-content-center">
+            <button className="getting-started btn btn-danger" type="button">
+              Get Started
+            </button>
           </div>
         </div>
-        </div>
-    </>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default ReceiptrentCcard
+export default ReceiptrentCcard;
+
